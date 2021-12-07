e8e05e041f0aa547be77ef721791b246e84ee68f
pato <pato9406@gmail.com>
2021-12-07 10:58:36 -0300

    Add solutions for Day 06

--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -3,6 +3,7 @@ import day02.Day02
 import day03.Day03
 import day04.Day04
 import day05.Day05
+import day06.Day06
 
 fun main() {
     Day01().main()
@@ -10,4 +11,5 @@ fun main() {
     Day03().main()
     Day04().main()
     Day05().main()
+    Day06().main()
 }
