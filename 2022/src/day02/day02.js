e56c4c89d8e0bc2c93b6e8052710eb3d99ce4375
pato <pato9406@gmail.com>
2022-12-17 12:57:54 -0300

    Replace broken algorithm with table

--- 2022/src/day02/day02.js
+++ 2022/src/day02/day02.js
@@ -1,31 +1,20 @@
-
-
-function wins(n, e) {
-    if (e === 3) { // scissors
-        return n === 1 // rock
-    }
-    return n > e
+const table = {
+    "B Z": 9,
+    "A Y": 8,
+    "C X": 7,
+    "C Z": 6,
+    "B Y": 5,
+    "A X": 4,
+    "A Z": 3,
+    "C Y": 2,
+    "B X": 1,
 }
 
 function solve1(input) {
     let score = 0
     for (const line of input) {
         if (line === "") continue
-        console.log(`line is ${line}`)
-        let [e, n] = line.split(" ").map((it) => it.charCodeAt(0))
-        e = e - "A".charCodeAt(0) + 1
-        n = n - "X".charCodeAt(0) + 1
-        if (wins(n, e)) {
-            console.log(`we win`)
-            score += 6
-        } else if (n === e) {
-            console.log(`we draw`)
-            score += 3
-        } else {
-            console.log(`we lose`)
-        }
-        score += n
-        console.log(`new score ${score}`)
+        score += table[line]
     }
     return score
 }
